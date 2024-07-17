ef9e15e6fe595d8251a21476fff8ab7c3050644e
Arne919 <ghgghg96@naver.com>
2024-07-17 11:47:59 +0900

    Add files via upload

---
+++ ws_2_c.py
@@ -0,0 +1,75 @@
+data = [{'has_more': False,
+  'next_cursor': None,
+  'object': 'list',
+  'page_or_database': {},
+  'request_id': 'a5163fff-758f-45ea-b6fb',
+  'results': [{'archived': False,
+               'cover': None,
+               'created_by': {'object': 'user'},
+               'created_time': '2023-06-15T04:29:00.000Z',
+               'icon': None,
+               'last_edited_by': {'object': 'user'},
+               'last_edited_time': '2023-12-12T09:19:00.000Z',
+               'object': 'page',
+               'parent': {'type': 'database_id'},
+               'properties': {'setNum': {'id': '%7DK%40%5C',
+                                         'number': 1,
+                                         'type': 'number'},
+                              '과목': {'id': 'YuIE',
+                                     'multi_select': [{'color': 'default',
+                                                       'name': 'Python'}],
+                                     'type': 'multi_select'},
+                              '구분': {'id': '%40%3EmR',
+                                     'select': {'color': 'purple',
+                                                'name': '실습'},
+                                     'type': 'select'},
+                              '단계': {'id': 'T%7B%7BP',
+                                     'select': {'color': 'default',
+                                                'name': '3'},
+                                     'type': 'select'},
+                              '문제번호': {'id': 'uEBt',
+                                       'number': 1431,
+                                       'type': 'number'},
+                              '제목': {'id': 'title',
+                                     'title': [{'annotations': {'bold': False,
+                                                                'code': False,
+                                                                'color': 'default',
+                                                                'italic': False,
+                                                                'strikethrough': False,
+                                                                'underline': False},
+                                                'href': None,
+                                                'plain_text': '복잡한 자료구조',
+                                                'text': {'content': '복잡한 자료구조',
+                                                         'link': None},
+                                                'type': 'text'}],
+                                     'type': 'title'},
+                              '일차': {'id': 'nWnH',
+                                     'number': '2',
+                                     'type': 'number'},
+                              '커리큘럼': {'id': 'T%3AR_',
+                                       'multi_select': [{'color': 'default',
+                                                         'name': 'fundamentals-of-python'}],
+                                       'type': 'multi_select'}},
+               'public_url': None
+            }],
+  'type': 'page_or_database'}]
+
+# first_data 아래 4가지 할당
+# 제목(text의 content)
+# 일차(일차_정수)
+# 단계(단계'{value}단계')
+# 과목()
+# first_data 출력
+title_content = data[0]['results'][0]['properties']['제목']['title'][0]['text']['content']
+day_number = data[0]['results'][0]['properties']['일차']['number']
+step_number = str(int(data[0]['results'][0]['properties']['단계']['select']['name']))+'단계'
+subject_name = data[0]['results'][0]['properties']['과목']['multi_select'][0]['name']
+first_data = {'제목': title_content, '일차': day_number, '단계': step_number, '과목': subject_name}
+print(first_data)
+
+
+
+
+
+
+
